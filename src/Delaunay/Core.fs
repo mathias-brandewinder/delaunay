10fca1f842d4ac0efebeabb8b6178713fed6f151
mathias-brandewinder <mathias@brandewinder.com>
2025-03-01 15:11:51 -0800

    First take: super triangle

--- src/Delaunay/Core.fs
+++ src/Delaunay/Core.fs
@@ -2,3 +2,50 @@
 
 type Point = { X: float; Y: float }
 
+type Triangle = {
+    A: Point
+    B: Point
+    C: Point
+    }
+
+module BowyerWatson =
+
+    let superTriangle (points: seq<Point>): Triangle =
+        
+        let xs =
+            points 
+            |> Seq.map (fun pt -> pt.X)
+            |> Array.ofSeq
+        let xMin = xs |> Array.min
+        let xMax = xs |> Array.max
+
+        let ys =
+            points
+            |> Seq.map (fun pt -> pt.Y)
+            |> Array.ofSeq
+        let yMin = ys |> Array.min
+        let yMax = ys |> Array.max
+
+        let squareWidth =
+            max (xMax - xMin) (yMax - yMin)
+        
+        let pointA = {
+            X = xMin - 0.5 * squareWidth
+            Y = yMin
+            }
+
+        let pointB = {
+            X = xMin + 1.5 * squareWidth
+            Y = yMin
+            }
+            
+        let pointC = {
+            X = xMin + 0.5 * squareWidth
+            Y = yMin + 2.0 * squareWidth
+            }
+
+        {
+            A = pointA
+            B = pointB
+            C = pointC
+        }
